310a2e5c844c49d076a888c510f52cf8c4f08b5d
Jonas Bromö <jonas.bromo@gmail.com>
2022-10-21 09:58:59 +0200

    Use ECB to fetch forex rates

--- EPWatch WatchKit Extension/API/ForexAPI.swift
+++ EPWatch WatchKit Extension/API/ForexAPI.swift
@@ -6,28 +6,30 @@
 //
 
 import Foundation
+import SwiftDate
 
 enum ForexError: Error {
-    case wrongBase
-    case missingRate
-    case ratesTooOld
+    case wrongFrom
+    case wrongTo
+    case outdatedRate
 }
 
 struct ForexLatest: Codable {
-    var base: String // "EUR"
     var date: String // "2022-09-07"
-    var rates: [String: Double] // { "SEK": 10.700151 }
+    var from: String // "EUR"
+    var to: String   // "SEK"
+    var rate: Double // 10.700151
 
     var isUpToDate: Bool {
-        return date.toDate("yyyy-MM-dd", region: .UTC)?.isToday == true
+        return date.toDate("yyyy-MM-dd")?.isToday == true
     }
 
     func rate(from: String, to: String) throws -> Double {
-        guard from == base else {
-            throw ForexError.wrongBase
+        guard from == from else {
+            throw ForexError.wrongFrom
         }
-        guard let rate = rates[to] else {
-            throw ForexError.missingRate
+        guard to == to else {
+            throw ForexError.wrongTo
         }
         return rate
     }
@@ -38,22 +40,57 @@ class ForexAPI {
 
     private init() {}
 
-    func download() async throws -> ForexLatest {
-        var components = URLComponents(string: "https://api.apilayer.com/fixer/latest")!
+    func download(from: String = "EUR", to: String = "SEK") async throws -> ForexLatest {
+        let yesterday = (DateInRegion() - 1.days)
+            .toFormat("yyyy-MM-dd")
+        var components = URLComponents(string: "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.\(to).\(from).SP00.A")!
         components.queryItems = [
-            URLQueryItem(name: "symbols", value: "SEK"), // Comma separated list
-            URLQueryItem(name: "base", value: "EUR"),
-            URLQueryItem(name: "apikey", value: "V7ChaSkcNEUXV2I0Z9ac4EqyzWkuNj7C")
+            URLQueryItem(name: "format", value: "csvdata"),
+            URLQueryItem(name: "startPeriod", value: yesterday)
         ]
-
         let (data, _) = try await URLSession.shared.data(from: components.url!)
         do {
-            let response = try JSONDecoder().decode(ForexLatest.self, from: data)
-            return response
+            guard let csv = String(data: data, encoding: .utf8) else {
+                throw NSError(0, "Failed to decode string from data")
+            }
+            let lines = csv.split(whereSeparator: \.isNewline)
+            guard lines.count >= 2 else {
+                throw NSError(0, "Failed, csv not 2 (or more) lines")
+            }
+            let values = lines[1].split(separator: ",")
+            guard values.count >= 7 else {
+                throw NSError(0, "Unexpected csv data, too few values")
+            }
+            guard values[2] == to, values[3] == from else {
+                throw NSError(0, "Unexpected csv data")
+            }
+            guard let rate = Double(values[7]) else {
+                throw NSError(0, "Unable to parse rate from csv data")
+            }
+            return ForexLatest(date: yesterday, from: from, to: to, rate: rate)
         } catch {
             LogError("Failed to parse: \(String(data: data, encoding: .utf8) ?? "")")
             throw error
         }
+
     }
+
+//    func download() async throws -> ForexLatest {
+//        var components = URLComponents(string: "https://api.apilayer.com/fixer/latest")!
+//        components.queryItems = [
+//            URLQueryItem(name: "symbols", value: "SEK"), // Comma separated list
+//            URLQueryItem(name: "base", value: "EUR"),
+//            URLQueryItem(name: "apikey", value: "V7ChaSkcNEUXV2I0Z9ac4EqyzWkuNj7C")
+//        ]
+//
+//        let (data, _) = try await URLSession.shared.data(from: components.url!)
+//        do {
+//            let response = try JSONDecoder().decode(ForexLatest.self, from: data)
+//            return response
+//        } catch {
+//            LogError("Failed to parse: \(String(data: data, encoding: .utf8) ?? "")")
+//            throw error
+//        }
+//    }
 
 }
