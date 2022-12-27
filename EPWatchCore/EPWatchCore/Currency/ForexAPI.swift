6d6cd5847e1cb39053f5d9f5f28a7097aed837d3
Jonas Bromö <jonas.bromo@gmail.com>
2022-12-27 16:21:55 +0100

    Fix bug failing to fetch exchange rate over holidays

--- EPWatchCore/EPWatchCore/Currency/ForexAPI.swift
+++ EPWatchCore/EPWatchCore/Currency/ForexAPI.swift
@@ -18,50 +18,54 @@ class ForexAPI {
     private init() {}
 
     func download(from: Currency, to: Currency) async throws -> ExchangeRate {
-        var startDate = Calendar.current.date(byAdding: .day, value: -1, to: .now)!
-        while Calendar.current.isDateInWeekend(startDate) {
-            startDate = Calendar.current.date(byAdding: .day, value: -1, to: startDate)!
-        }
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let startPeriod = dateFormatter.string(from: startDate)
+        let maxNumberOfTries = 7
+        for attempt in 1...maxNumberOfTries {
+            let startDate = Calendar.current.date(byAdding: .day, value: -attempt, to: .now)!
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let startPeriod = dateFormatter.string(from: startDate)
 
-        guard from != to else {
-            return ExchangeRate(date: startPeriod, from: from, to: to, rate: 1)
-        }
+            guard from != to else {
+                return ExchangeRate(date: startPeriod, from: from, to: to, rate: 1)
+            }
 
-        var components = URLComponents(string: "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.\(to).\(from).SP00.A")!
-        components.queryItems = [
-            URLQueryItem(name: "format", value: "csvdata"),
-            URLQueryItem(name: "startPeriod", value: startPeriod)
-        ]
-        let downloadResponse = await AF.download(components.url!).serializingData().response
-        let data = try downloadResponse.result.get()
-        let statusCode = downloadResponse.response?.statusCode
-        do {
-            guard let csv = String(data: data, encoding: .utf8) else {
-                throw NSError(0, "Failed to decode string from data")
-            }
-            let lines = csv.split(whereSeparator: \.isNewline)
-            guard lines.count >= 2 else {
-                throw NSError(0, "Failed, csv not 2 (or more) lines")
-            }
-            let values = lines[1].split(separator: ",")
-            guard values.count >= 7 else {
-                throw NSError(0, "Unexpected csv data, too few values")
-            }
-            guard values[2] == to.code, values[3] == from.code else {
-                throw NSError(0, "Unexpected csv data")
-            }
-            guard let rate = Double(values[7]) else {
-                throw NSError(0, "Unable to parse rate from csv data")
-            }
-            return ExchangeRate(date: startPeriod, from: from, to: to, rate: rate)
-        } catch {
-            LogError("Failed to parse: \(String(data: data, encoding: .utf8) ?? ""), statusCode: \(statusCode ?? 0)")
-            throw error
-        }
+            var components = URLComponents(string: "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.\(to).\(from).SP00.A")!
+            components.queryItems = [
+                URLQueryItem(name: "format", value: "csvdata"),
+                URLQueryItem(name: "startPeriod", value: startPeriod)
+            ]
 
+            var statusCode: Int?
+            var responseString: String?
+            do {
+                let downloadResponse = await AF.download(components.url!).serializingData().response
+                let data = try downloadResponse.result.get()
+                statusCode = downloadResponse.response?.statusCode
+
+                guard let csv = String(data: data, encoding: .utf8) else {
+                    throw NSError(0, "Failed to decode string from data")
+                }
+                responseString = csv
+                let lines = csv.split(whereSeparator: \.isNewline)
+                guard lines.count >= 2 else {
+                    throw NSError(0, "Failed, csv not 2 (or more) lines")
+                }
+                let values = lines[1].split(separator: ",")
+                guard values.count >= 7 else {
+                    throw NSError(0, "Unexpected csv data, too few values")
+                }
+                guard values[2] == to.code, values[3] == from.code else {
+                    throw NSError(0, "Unexpected csv data")
+                }
+                guard let rate = Double(values[7]) else {
+                    throw NSError(0, "Unable to parse rate from csv data")
+                }
+                return ExchangeRate(date: startPeriod, from: from, to: to, rate: rate)
+            } catch {
+                LogError("Failed to fetch exchange rate for date \(startPeriod), attempt \(attempt), error: \(error), response: \(responseString ?? ""), statusCode: \(statusCode ?? 0)")
+            }
+        }
+        throw NSError(0, "Failed to fetch exchange rate in \(maxNumberOfTries) attempts")
     }
 
 }
